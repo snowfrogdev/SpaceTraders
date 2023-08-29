65c54826f39d06f43e16aad1fb0ba0dc1c870f78
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2023-08-28 22:22:04 -0400

    Clamp camera beta rotation

--- Prototypes/BabylonJS-With-Editor/declaration/src/scenes/scripts-map.d.ts
+++ Prototypes/BabylonJS-With-Editor/declaration/src/scenes/scripts-map.d.ts
@@ -1,7 +1,9 @@
+import { ScriptMap } from "./tools";
 /**
  * Defines the interface that exposes all exported scripts in this project.
  */
 export interface ISceneScriptMap {
+    "src/scenes/scene/camera.ts": ScriptMap;
 }
 /**
  * Defines the map of all available scripts in the project.
---
+++ Prototypes/BabylonJS-With-Editor/src/scenes/scene/camera.ts
@@ -0,0 +1,67 @@
+import { ArcRotateCamera, Tools } from "@babylonjs/core";
+import { visibleInInspector } from "../decorators";
+
+export default class MyScript extends ArcRotateCamera {
+    @visibleInInspector("number", "lowerBetaLimit", 0.1, { min: -Math.PI, max: Math.PI })
+    lowerBetaLimit: number;
+    @visibleInInspector("number", "upperBetaLimit", Tools.ToRadians(30), { min: -Math.PI, max: Math.PI })
+    upperBetaLimit: number;
+
+    /**
+     * Override constructor.
+     * @warn do not fill.
+     */
+    // @ts-ignore ignoring the super call as we don't want to re-init
+    protected constructor() { }
+
+    /**
+     * Called on the node is being initialized.
+     * This function is called immediatly after the constructor has been called.
+     */
+    public onInitialize(): void {
+
+    }
+
+    /**
+     * Called on the node has been fully initialized and is ready.
+     */
+    public onInitialized(): void {
+        // ...
+    }
+
+    /**
+     * Called on the scene starts.
+     */
+    public onStart(): void {
+        // ...
+    }
+
+    /**
+     * Called each frame.
+     */
+    public onUpdate(): void {
+        // ...
+    }
+
+    /**
+     * Called on the object has been disposed.
+     * Object can be disposed manually or when the editor stops running the scene.
+     */
+    public onStop(): void {
+        // ...
+    }
+
+    /**
+     * Called on a message has been received and sent from a graph.
+     * @param name defines the name of the message sent from the graph.
+     * @param data defines the data sent in the message.
+     * @param sender defines the reference to the graph class that sent the message.
+     */
+    public onMessage(name: string, data: any, sender: any): void {
+        switch (name) {
+            case "myMessage":
+                // Do something...
+                break;
+        }
+    }
+}
